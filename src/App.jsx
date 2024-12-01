1e050bec84e30f54fe480387ce7cd98475be9659
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2024-12-01 23:42:18 +0700

    add code splitting in route

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
 import NavbarComponent from "./components/NavbarComponent";
 import FooterComponent from "./components/FooterComponent";
-import DetailPage from "./pages/DetailPage";
-import NewslatterPage from "./pages/NewsLatterPage";
-import NotFound from "./pages/NotFound";
+const Home = lazy(() => import("./pages/Home"));
+const DetailPage = lazy(() => import("./pages/DetailPage"));
+const NewslatterPage = lazy(() => import("./pages/NewsLatterPage"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <Router>
       <NavbarComponent />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/blog/*" element={<DetailPage />} />
-        <Route path="/newsletter" element={<NewslatterPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/blog/*" element={<DetailPage />} />
+          <Route path="/newsletter" element={<NewslatterPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
       <FooterComponent />
     </Router>
   );
