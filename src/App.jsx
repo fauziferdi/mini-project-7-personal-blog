2e8fde6e91baf3d63b125ef6430151dfa6417c94
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2024-12-01 11:52:45 +0700

    fixed redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import NavbarComponent from "./components/NavbarComponent";
-
 import FooterComponent from "./components/FooterComponent";
 import DetailPage from "./pages/DetailPage";
 
@@ -11,7 +10,7 @@ function App() {
       <NavbarComponent />
       <Routes>
         <Route path="*" element={<Home />} />
-        <Route path="/blog/:id" element={<DetailPage />} />
+        <Route path="/blog/*" element={<DetailPage />} />
       </Routes>
       <FooterComponent />
     </Router>
